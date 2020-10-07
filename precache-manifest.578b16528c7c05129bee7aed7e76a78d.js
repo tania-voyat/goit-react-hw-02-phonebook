757d6b1060161358d0f4740633fb4afa219afb61
tania.voyat <tetiana.voyat@gmail.com>
2020-10-07 15:03:41 +0300

    Updates

---
+++ precache-manifest.578b16528c7c05129bee7aed7e76a78d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3125ca44edad0d3bdf1f31cc13d5f3d",
+    "url": "/goit-react-hw-02-phonebook/index.html"
+  },
+  {
+    "revision": "4d3fb9cd10c0069a6636",
+    "url": "/goit-react-hw-02-phonebook/static/css/main.a896a305.chunk.css"
+  },
+  {
+    "revision": "1095ac2dd4109899aa99",
+    "url": "/goit-react-hw-02-phonebook/static/js/2.7063ec41.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-02-phonebook/static/js/2.7063ec41.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d3fb9cd10c0069a6636",
+    "url": "/goit-react-hw-02-phonebook/static/js/main.f3f2f93a.chunk.js"
+  },
+  {
+    "revision": "65c8a2f1bf8ffcc01bde",
+    "url": "/goit-react-hw-02-phonebook/static/js/runtime-main.d8bf734b.js"
+  }
+]);
